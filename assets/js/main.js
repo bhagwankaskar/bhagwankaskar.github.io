805f48fa121fc43df50dbfb34e6245f065ebb3e0
deeptikaskar <deepti@symboinsurance.com>
2020-11-05 11:48:58 +0530

    sg16: Added our offerings banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,28 @@
 
 			const preload = document.querySelector('.preload');
 			const logo = document.querySelectorAll('#preloadLogo path');
+			if (matchMedia("(max-width: 700px)").matches) {
+				$('#svgfile').addClass('pos_mobile')
+				$('#svgfile').load('./images/mobileLogo.svg');
+				// css changes for mobile view
+				$("#svgfile").css("position", "absolute");
+				$("#svgfile").css("left", "12%");
+				$("#svgfile").css("top", "32%");
+	
+				$('#spin_circle').addClass('pos_mobile')
+				$('#spin_circle1').addClass('pos1_mobile')
+				$('#preloadLogo').addClass('preload_text')
+				$('#logo_shree_ganesh').addClass('shree_ganesh_mobile')
+	
+	
+	
+			} else {
+				$('#svgfile').addClass('mb20')
+				$("#svgfile").css("position", "absolute");
+				$("#svgfile").css("left", "14%");
+				$("#svgfile").css("top", "43%");
+				$('#svgfile').load('./images/desktopLogo.svg');
+			}
 			console.log({logo})
 		// for(let i=0;i< logo.length; i++){
 		// 	console.log('---> length', `${i} is ${logo[i].getTotalLength()}`)
@@ -32,33 +54,14 @@
 			// 	$body.removeClass('is-preload');
 			// }, 0);
 
-			window.setTimeout(function () {
-				preload.classList.add('preload-finish');
-			}, 5000);
+			
 
 	
-		if (matchMedia("(max-width: 700px)").matches) {
-			$('#svgfile').addClass('pos_mobile')
-			$('#svgfile').load('./images/mobileLogo.svg');
-			// css changes for mobile view
-			$("#svgfile").css("position", "absolute");
-			$("#svgfile").css("left", "12%");
-			$("#svgfile").css("top", "32%");
+		
 
-			$('#spin_circle').addClass('pos_mobile')
-			$('#spin_circle1').addClass('pos1_mobile')
-			$('#preloadLogo').addClass('preload_text')
-			$('#logo_shree_ganesh').addClass('shree_ganesh_mobile')
-
-
-
-		} else {
-			$('#svgfile').addClass('mb20')
-			$("#svgfile").css("position", "absolute");
-			$("#svgfile").css("left", "14%");
-			$("#svgfile").css("top", "43%");
-			$('#svgfile').load('./images/desktopLogo.svg');
-		}
+		window.setTimeout(function () {
+			preload.classList.add('preload-finish');
+		}, 5000);
 	});
 
 	// Dropdowns.
